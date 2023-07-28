57facf15bb8a5f1a87c784508f5e3d5ad5a8e6bc
Vihan Perera <vihanpereraux@gmail.com>
2023-07-28 18:21:38 +0530

    002 - Update script

--- 002-atomic-bomb/sketch.js
+++ 002-atomic-bomb/sketch.js
@@ -1,7 +1,37 @@
+let devider = 3;
+let canvasColor = 30;
+let mideaElement = document.getElementById('clip');
+let randomTimeStamp;
+let randomizer;
+let intervalOne;
+let intervalTwo;
+let footageNames = [];
+let intervalCounter;
+
+intervalOne = setInterval(() => {
+  randomizer = round(random(-50, 50));
+  randomTimeStamp = round(random(0, 3*60) + randomizer);
+  console.log(randomTimeStamp);
+  mideaElement.currentTime = randomTimeStamp;
+}, 250);
+
+footageNames = [
+  'footage-one.mp4',
+  'footage-two.mp4',
+  'footage-three.mp4',
+]
+intervalCounter = 10;
+
+intervalTwo = setInterval(() => {
+  mideaElement.src = './assets/vids/' + footageNames[round(random(0, 2))];
+  intervalCounter = intervalCounter + 5;
+}, 4000);
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1080/devider, 1920/devider);
+  background(canvasColor);
 }
 
 function draw() {
-  background(220);
+
 }
