6cea439ddd2cd03c54f5b3d8e0049829206a302e
Vihan Perera <vihanpereraux@gmail.com>
2023-07-28 16:43:37 +0530

    Add track js

--- 001-vintage-images/assets/js/track.js
+++ 001-vintage-images/assets/js/track.js
@@ -4,22 +4,31 @@ const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 let trackButton = document.getElementById("trackbutton");
 let updateNote = document.getElementById("updatenote");
+let detailLogWrapper = document.getElementById("detail-log-wrapper");
 
 let isVideo = false;
 let model = null;
 
 const modelParams = {
     flipHorizontal: true,   // flip e.g for video  
-    maxNumBoxes: 20,        // maximum number of boxes to detect
+    maxNumBoxes: 2,        // maximum number of boxes to detect
     iouThreshold: 0.5,      // ioU threshold for non-max suppression
     scoreThreshold: 0.6,    // confidence threshold for predictions.
 }
 
+function toggleVideo() {
+    if (!isVideo) {
+        startVideo();
+    } else {
+        handTrack.stopVideo(video)
+        isVideo = false;
+    }
+}
+
 function startVideo() {
     handTrack.startVideo(video).then(function (status) {
-        console.log("video started", status);
+        // console.log("video started", status);
         if (status) {
-            // updateNote.innerText = "Video started. Now tracking"
             isVideo = true
             runDetection()
         } else {
@@ -28,23 +37,24 @@ function startVideo() {
     });
 }
 
-function toggleVideo() {
-    if (!isVideo) {
-        // updateNote.innerText = "Starting video"
-        startVideo();
-    } else {
-        // updateNote.innerText = "Stopping video"
-        handTrack.stopVideo(video)
-        isVideo = false;
-        // updateNote.innerText = "Video stopped"
-    }
-}
-
-
+// Load the model.
+handTrack.load(modelParams).then(lmodel => {
+    model = lmodel
+    trackButton.disabled = false
+});
 
 function runDetection() {
     model.detect(video).then(predictions => {
-        console.log("Predictions: ", predictions);
+        if(predictions.length == 0){
+            console.log("Predictions: No results");
+        }
+        else{
+            console.log("Predictions: ", predictions[0].bbox);
+            console.log("Predictions: ", predictions[0].class);
+            console.log("Predictions: ", predictions[0].label);
+            console.log("Predictions: ", predictions[0].score);
+        }
+        // detailLogWrapper.innerText = predictions[0].class;
         model.renderPredictions(predictions, canvas, context, video);
         if (isVideo) {
             requestAnimationFrame(runDetection);
@@ -52,10 +62,3 @@ function runDetection() {
     });
 }
 
-// Load the model.
-handTrack.load(modelParams).then(lmodel => {
-    // detect objects in the image.
-    model = lmodel
-    // updateNote.innerText = "Loaded Model!"
-    trackButton.disabled = false
-});
