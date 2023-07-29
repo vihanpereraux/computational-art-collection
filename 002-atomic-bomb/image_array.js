6c958a8e3878bef20956e5b567001985fba198fd
Vihan Perera <vihanpereraux@gmail.com>
2023-07-29 13:40:24 +0530

    002 - Update script with effects and stat

---
+++ 002-atomic-bomb/image_array.js
@@ -0,0 +1,15 @@
+let citeImages = [
+    ['./assets/images/alamogordo/img-01.jpg' ,'alamogordo'],
+    ['./assets/images/alamogordo/img-02.jpg' ,'alamogordo'],
+    ['./assets/images/alamogordo/img-03.jpg' ,'alamogordo'],
+    ['./assets/images/alamogordo/img-04.jpg' ,'alamogordo'],
+    ['./assets/images/alamogordo/img-05.jpg' ,'alamogordo'],
+    ['./assets/images/alamogordo/img-06.jpg' ,'alamogordo'],
+  
+    ['./assets/images/hiroshima/img-01.jpg' ,'hiroshima'],
+    ['./assets/images/hiroshima/img-02.jpg' ,'hiroshima'],
+    ['./assets/images/hiroshima/img-03.jpg' ,'hiroshima'],
+    ['./assets/images/hiroshima/img-04.jpg' ,'hiroshima'],
+    ['./assets/images/hiroshima/img-05.jpg' ,'hiroshima'],
+    ['./assets/images/hiroshima/img-06.jpg' ,'hiroshima'],
+  ];
--- 002-atomic-bomb/sketch.js
+++ 002-atomic-bomb/sketch.js
@@ -21,6 +21,11 @@ let randomCity;
 let image01 = document.getElementById('image-01');
 let image02 = document.getElementById('image-02');
 let image03 = document.getElementById('image-03');
+let paperTapeOne = document.getElementById('paper-tape-one');
+let paperTapeTwo = document.getElementById('paper-tape-two');
+let year = document.getElementById('year');
+let yeildLower = document.getElementById('yeild-lower');
+let yeildUpper = document.getElementById('yeild-upper');
 
 intervalOne = setInterval(() => {
   randomizer = round(random(-50, 50));
@@ -40,26 +45,13 @@ intervalTwo = setInterval(() => {
 
 intervalThree = setInterval(() => {
   image01.style.display = 'none';
-  // image02.style.display = 'block';
   image03.style.display = 'block';
 }, 2000);
 intervalFour = setInterval(() => {
   image01.style.display = 'block';
   image03.style.display = 'none';
 }, 4000);
-intervalFive = setInterval(() => {
-  // image02.style.display = 'none';
-}, 6000);
 
-
-let citeImages = [
-  ['./assets/images/alamogordo/img-01.jpg' ,'alamogordo'],
-  ['./assets/images/alamogordo/img-02.jpg' ,'alamogordo'],
-  ['./assets/images/alamogordo/img-03.jpg' ,'alamogordo'],
-  ['./assets/images/alamogordo/img-04.jpg' ,'alamogordo'],
-  ['./assets/images/alamogordo/img-05.jpg' ,'alamogordo'],
-  ['./assets/images/alamogordo/img-06.jpg' ,'alamogordo'],
-];
 
 function preload(){
   dataTable = loadTable('assets/data/data.csv');
@@ -75,21 +67,31 @@ function setup() {
 
 function draw() {
   let randomRowCount = round(random(0, dataTable.getRowCount()));
+  
   randomCity = dataTable.getString(randomRowCount, 1);
+  
   let location = {
     Latitude: dataTable.getString(randomRowCount, 3),
-    Longitude: dataTable.getString(randomRowCount, 4)
+    Longitude: dataTable.getString(randomRowCount, 4),
+    date: dataTable.getString(randomRowCount, 13) + " " +
+     dataTable.getString(randomRowCount, 14) + " " + 
+     dataTable.getString(randomRowCount, 15),
+    yeildLower: dataTable.getString(randomRowCount, 8),
+    yeildUpper: dataTable.getString(randomRowCount, 9)
   }
-  console.log(randomCity)
-  city.innerText = randomCity;
+  console.log(location);
 
-  latitude.innerHTML = location.Latitude;
-  longitude.innerHTML = location.Longitude;
+  city.innerText = randomCity;
+  latitude.innerText = location.Latitude;
+  longitude.innerText = location.Longitude;
+  year.innerText = location.date;
+  yeildLower.innerText = location.yeildLower;
+  yeildUpper.innerText = location.yeildUpper;
 
   let multiplierOne = (round(random(-15, 15)));
   let multiplierTwo = (round(random(10, -10)));
   noFill();
-  stroke(255);
+  stroke(255, 0, 0);
   strokeWeight(.3);
   circle(
     location.Latitude * multiplierOne,
@@ -97,7 +99,8 @@ function draw() {
     40
   );
 
-  fill(255);
+  fill(255, 0, 0);
+  noStroke();
   circle(
     location.Latitude * multiplierOne,
     location.Longitude * multiplierTwo,
@@ -115,8 +118,12 @@ function draw() {
   );
   
   if(true){
-    image01.src = citeImages[round(random(0, 5))][0];
-    image02.src = citeImages[round(random(0, 5))][0];
-    image03.src = citeImages[round(random(0, 5))][0];
+    image01.src = citeImages[round(random(0, citeImages.length-1))][0];
+    image02.src = citeImages[round(random(0, citeImages.length-1))][0];
+    image03.src = citeImages[round(random(0, citeImages.length-1))][0];
   }
+
+  let rotatevalue = round(random(2,-2));
+  paperTapeOne.style.transform = 'translateX(-50%) rotate(' +  rotatevalue * random(-2, 2)  + 'deg)';
+  paperTapeTwo.style.transform = 'translateX(-50%) rotate(' +  rotatevalue  + 'deg)';
 }
