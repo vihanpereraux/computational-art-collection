a46faed1a04d16855c21a120cf54c7a546c78cac
Vihan Perera <vihanpereraux@gmail.com>
2023-08-27 13:06:17 +0100

    add block one

--- 004-good-bye-all/sketch.js
+++ 004-good-bye-all/sketch.js
@@ -1,9 +1,21 @@
+// block one
+let blockOneKeyImage;
+let blockOneOtherImages;
+
 let devider = 3;
 
 function setup() {
   createCanvas(1080/devider, 1920/devider);
+  background(220);
 }
 
 function draw() {
-  background(220);
+  blockOne();
+  // fill(255, 0, 0)
+  // circle(mouseX, mouseY, 20, 20)
+}
+
+function blockOne(){
+  blockOneKeyImage = document.getElementById('block-one-key-image');
+  blockOneKeyImage.src = 'https://i.ibb.co/d6mFXvg/img-001.png';
 }
