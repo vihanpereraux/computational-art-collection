357cff4143c930c4e4a5df09baa92381c4147c09
Vihan Perera <vihanpereraux@gmail.com>
2023-09-02 12:10:57 +0100

    005 - Update sketch

--- 005-london-electricity-data-viz/sketch.js
+++ 005-london-electricity-data-viz/sketch.js
@@ -14,12 +14,12 @@ let randomHeight;
 
 let blockNumber;
 
-function preload(){
+function preload() {
   dataTable = loadTable('source/data/historic_demand_year_2023.csv');
 }
 
 function setup() {
-  frameRate(8);  
+  frameRate(8);
   createCanvas(360, 640);
   blockNumber = localStorage.getItem('blockNumber');
 }
@@ -29,24 +29,24 @@ function draw() {
 
   switch (blockNumber) {
     case "1":
-      blockOne();    
+      blockOne();
       break;
-  
+
     case "2":
-      blockTwo();    
+      blockTwo();
       break;
 
     case "3":
-      blockThree();    
+      blockThree();
       break;
-    
+
     case "4":
-      blockFour();    
+      blockFour();
       break;
   }
 }
 
-function blockOne(){
+function blockOne() {
   noFill();
   stroke(255);
   strokeWeight(.7);
@@ -56,82 +56,10 @@ function blockOne(){
   randomWidth = random(80, 110);
   randomHeight = random(80, 110);
 
-  // get relative pointsss : ) 
-  randomSettlementDate = Math.round(random(1, dataTable.getRowCount()));
-
-  // line one
-  line(
-    0, 
-    randomYValue + randomHeight/2, 
-    randomXValue, 
-    randomYValue + randomHeight/2);
-
-  // line two
-  line(
-    randomXValue + randomWidth/2, 
-    randomYValue + randomHeight, 
-    randomXValue + randomWidth/2, 
-    height);
-
-  // line three
-  line(
-    randomXValue + randomWidth, 
-    randomYValue + randomHeight/2, 
-    width, 
-    randomYValue + randomHeight/2);
-
-  // line four
-  line(
-    randomXValue + randomWidth/2, 
-    randomYValue, 
-    randomXValue + randomWidth/2, 
-    0);
-
-  englandWalesDemand = dataTable.getString(randomSettlementDate, 4);
-  if(englandWalesDemand > 20000){
-    stroke(255, 0, 0);
-    strokeWeight(1);
-    rect(
-      randomXValue, 
-      randomYValue, 
-      randomWidth, 
-      randomHeight);
-
-    fill(255, 0, 0);
-    strokeWeight(.5);
-    text(englandWalesDemand, (randomXValue + randomWidth/2) - 10, randomYValue + randomHeight/2);
-  }
-  else{
-    stroke(255);
-    strokeWeight(1);
-    rect(
-      randomXValue, 
-      randomYValue, 
-      randomWidth, 
-      randomHeight);
-
-    fill(255);
-    strokeWeight(.5);
-    text(englandWalesDemand, (randomXValue + randomWidth/2) - 10, randomYValue + randomHeight/2);
-  }
-
-  embeddedWindGeneration = dataTable.getString(randomSettlementDate, 5);
-  embeddedWindCapacity = dataTable.getString(randomSettlementDate, 6);
-  embeddedSolorGeneration = dataTable.getString(randomSettlementDate, 7);
-  embeddedSolorCapacity = dataTable.getString(randomSettlementDate, 8);
-
-  textSize(10);
-  fill(255);
-  stroke(255);
-  strokeWeight(.5);
-
-  text(embeddedWindGeneration, randomXValue, randomYValue - 10);
-  text(embeddedWindCapacity, (randomXValue + randomWidth) - 20, randomYValue - 10);
-  text(embeddedSolorGeneration, randomXValue, (randomYValue + randomHeight) + 15);
-  text(embeddedSolorCapacity, (randomXValue + randomWidth) - 25, (randomYValue + randomHeight) + 15);
+  getIllustration();
 }
 
-function blockTwo(){
+function blockTwo() {
   noFill();
   stroke(255);
   strokeWeight(.7);
@@ -141,82 +69,10 @@ function blockTwo(){
   randomWidth = random(90, 120);
   randomHeight = random(90, 120);
 
-  // get relative pointsss : ) 
-  randomSettlementDate = Math.round(random(1, dataTable.getRowCount()));
-
-  // line one
-  line(
-    0, 
-    randomYValue + randomHeight/2, 
-    randomXValue, 
-    randomYValue + randomHeight/2);
-
-  // line two
-  line(
-    randomXValue + randomWidth/2, 
-    randomYValue + randomHeight, 
-    randomXValue + randomWidth/2, 
-    height);
-
-  // line three
-  line(
-    randomXValue + randomWidth, 
-    randomYValue + randomHeight/2, 
-    width, 
-    randomYValue + randomHeight/2);
-
-  // line four
-  line(
-    randomXValue + randomWidth/2, 
-    randomYValue, 
-    randomXValue + randomWidth/2, 
-    0);
-
-  englandWalesDemand = dataTable.getString(randomSettlementDate, 4);
-  if(englandWalesDemand > 20000){
-    stroke(255, 0, 0);
-    strokeWeight(1);
-    rect(
-      randomXValue, 
-      randomYValue, 
-      randomWidth, 
-      randomHeight);
-
-    fill(255, 0, 0);
-    strokeWeight(.5);
-    text(englandWalesDemand, (randomXValue + randomWidth/2) - 10, randomYValue + randomHeight/2);
-  }
-  else{
-    stroke(255);
-    strokeWeight(1);
-    rect(
-      randomXValue, 
-      randomYValue, 
-      randomWidth, 
-      randomHeight);
-
-    fill(255);
-    strokeWeight(.5);
-    text(englandWalesDemand, (randomXValue + randomWidth/2) - 10, randomYValue + randomHeight/2);
-  }
-
-  embeddedWindGeneration = dataTable.getString(randomSettlementDate, 5);
-  embeddedWindCapacity = dataTable.getString(randomSettlementDate, 6);
-  embeddedSolorGeneration = dataTable.getString(randomSettlementDate, 7);
-  embeddedSolorCapacity = dataTable.getString(randomSettlementDate, 8);
-
-  textSize(10);
-  fill(255);
-  stroke(255);
-  strokeWeight(.5);
-
-  text(embeddedWindGeneration, randomXValue, randomYValue - 10);
-  text(embeddedWindCapacity, (randomXValue + randomWidth) - 20, randomYValue - 10);
-  text(embeddedSolorGeneration, randomXValue, (randomYValue + randomHeight) + 15);
-  text(embeddedSolorCapacity, (randomXValue + randomWidth) - 25, (randomYValue + randomHeight) + 15);
+  getIllustration();
 }
 
-function blockThree(){
+function blockThree() {
   noFill();
   stroke(255);
   strokeWeight(.7);
@@ -226,82 +82,10 @@ function blockThree(){
   randomWidth = random(140, 120);
   randomHeight = random(140, 120);
 
-  // get relative pointsss : ) 
-  randomSettlementDate = Math.round(random(1, dataTable.getRowCount()));
-
-  // line one
-  line(
-    0, 
-    randomYValue + randomHeight/2, 
-    randomXValue, 
-    randomYValue + randomHeight/2);
-
-  // line two
-  line(
-    randomXValue + randomWidth/2, 
-    randomYValue + randomHeight, 
-    randomXValue + randomWidth/2, 
-    height);
-
-  // line three
-  line(
-    randomXValue + randomWidth, 
-    randomYValue + randomHeight/2, 
-    width, 
-    randomYValue + randomHeight/2);
-
-  // line four
-  line(
-    randomXValue + randomWidth/2, 
-    randomYValue, 
-    randomXValue + randomWidth/2, 
-    0);
-
-  englandWalesDemand = dataTable.getString(randomSettlementDate, 4);
-  if(englandWalesDemand > 20000){
-    stroke(255, 0, 0);
-    strokeWeight(1);
-    rect(
-      randomXValue, 
-      randomYValue, 
-      randomWidth, 
-      randomHeight);
-
-    fill(255, 0, 0);
-    strokeWeight(.5);
-    text(englandWalesDemand, (randomXValue + randomWidth/2) - 10, randomYValue + randomHeight/2);
-  }
-  else{
-    stroke(255);
-    strokeWeight(1);
-    rect(
-      randomXValue, 
-      randomYValue, 
-      randomWidth, 
-      randomHeight);
-
-    fill(255);
-    strokeWeight(.5);
-    text(englandWalesDemand, (randomXValue + randomWidth/2) - 10, randomYValue + randomHeight/2);
-  }
-
-  embeddedWindGeneration = dataTable.getString(randomSettlementDate, 5);
-  embeddedWindCapacity = dataTable.getString(randomSettlementDate, 6);
-  embeddedSolorGeneration = dataTable.getString(randomSettlementDate, 7);
-  embeddedSolorCapacity = dataTable.getString(randomSettlementDate, 8);
-
-  textSize(10);
-  fill(255);
-  stroke(255);
-  strokeWeight(.5);
-
-  text(embeddedWindGeneration, randomXValue, randomYValue - 10);
-  text(embeddedWindCapacity, (randomXValue + randomWidth) - 20, randomYValue - 10);
-  text(embeddedSolorGeneration, randomXValue, (randomYValue + randomHeight) + 15);
-  text(embeddedSolorCapacity, (randomXValue + randomWidth) - 25, (randomYValue + randomHeight) + 15);
+  getIllustration();
 }
 
-function blockFour(){
+function blockFour() {
   noFill();
   stroke(255);
   strokeWeight(.7);
@@ -311,63 +95,67 @@ function blockFour(){
   randomWidth = random(130, 110);
   randomHeight = random(130, 110);
 
+  getIllustration();
+}
+
+function getIllustration() {
   // get relative pointsss : ) 
   randomSettlementDate = Math.round(random(1, dataTable.getRowCount()));
 
   // line one
   line(
-    0, 
-    randomYValue + randomHeight/2, 
-    randomXValue, 
-    randomYValue + randomHeight/2);
+    0,
+    randomYValue + randomHeight / 2,
+    randomXValue,
+    randomYValue + randomHeight / 2);
 
   // line two
   line(
-    randomXValue + randomWidth/2, 
-    randomYValue + randomHeight, 
-    randomXValue + randomWidth/2, 
+    randomXValue + randomWidth / 2,
+    randomYValue + randomHeight,
+    randomXValue + randomWidth / 2,
     height);
 
   // line three
   line(
-    randomXValue + randomWidth, 
-    randomYValue + randomHeight/2, 
-    width, 
-    randomYValue + randomHeight/2);
+    randomXValue + randomWidth,
+    randomYValue + randomHeight / 2,
+    width,
+    randomYValue + randomHeight / 2);
 
   // line four
   line(
-    randomXValue + randomWidth/2, 
-    randomYValue, 
-    randomXValue + randomWidth/2, 
+    randomXValue + randomWidth / 2,
+    randomYValue,
+    randomXValue + randomWidth / 2,
     0);
 
   englandWalesDemand = dataTable.getString(randomSettlementDate, 4);
-  if(englandWalesDemand > 20000){
+  if (englandWalesDemand > 20000) {
     stroke(255, 0, 0);
     strokeWeight(1);
     rect(
-      randomXValue, 
-      randomYValue, 
-      randomWidth, 
+      randomXValue,
+      randomYValue,
+      randomWidth,
       randomHeight);
 
     fill(255, 0, 0);
     strokeWeight(.5);
-    text(englandWalesDemand, (randomXValue + randomWidth/2) - 10, randomYValue + randomHeight/2);
+    text(englandWalesDemand, (randomXValue + randomWidth / 2) - 10, randomYValue + randomHeight / 2);
   }
-  else{
+  else {
     stroke(255);
     strokeWeight(1);
     rect(
-      randomXValue, 
-      randomYValue, 
-      randomWidth, 
+      randomXValue,
+      randomYValue,
+      randomWidth,
       randomHeight);
 
     fill(255);
     strokeWeight(.5);
-    text(englandWalesDemand, (randomXValue + randomWidth/2) - 10, randomYValue + randomHeight/2);
+    text(englandWalesDemand, (randomXValue + randomWidth / 2) - 10, randomYValue + randomHeight / 2);
   }
 
   embeddedWindGeneration = dataTable.getString(randomSettlementDate, 5);
