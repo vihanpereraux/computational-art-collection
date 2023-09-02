d9e7050a0d21ebdb6a3a488c44747309603f8568
Vihan Perera <vihanpereraux@gmail.com>
2023-09-02 08:02:40 +0100

    005 - Add block one

--- 005-london-electricity-data-viz/sketch.js
+++ 005-london-electricity-data-viz/sketch.js
@@ -1,45 +1,43 @@
 let dataTable;
 
-let englandWalesDeman;
+let randomSettlementDate;
+let englandWalesDemand;
 let embeddedWindGeneration;
 let embeddedWindCapacity;
 let embeddedSolorGeneration;
 let embeddedSolorCapacity;
-
-function preload(){
-  dataTable = loadTable('source/data/historic_demand_year_2023.csv');
-}
-
-function setup() {
-  frameRate(6);
-
-  console.log("Row Count" , dataTable.getRowCount());
-  console.log("Column Count" , dataTable.getColumnCount());
-  console.log("Data" , dataTable.getString(0, 4));
-
-  // for (let index = 1; index < dataTable.getRowCount(); index++) {
-  //   console.log(dataTable.getString(index, 4));
-  // }
-  
-  createCanvas(360, 640);
-}
 
 let randomXValue;
 let randomYValue;
 let randomWidth;
 let randomHeight;
 
-function draw() {
-  clear()
+function preload(){
+  dataTable = loadTable('source/data/historic_demand_year_2023.csv');
+}
 
+function setup() {
+  frameRate(6);  
+  createCanvas(360, 640);
+}
+
+function draw() {
+  clear();
+  blockOne();
+}
+
+function blockOne(){
   noFill();
   stroke(255);
   strokeWeight(.7);
 
-  randomXValue = random(90, 120);
+  randomXValue = random(80, 120);
   randomYValue = random(170, 210);
-  randomWidth = random(70, 100);
-  randomHeight = random(80, 100);
+  randomWidth = random(80, 110);
+  randomHeight = random(80, 110);
+
+  // get relative pointsss : ) 
+  randomSettlementDate = Math.round(random(1, dataTable.getRowCount()));
 
   // line one
   line(
@@ -69,22 +67,45 @@ function draw() {
     randomXValue + randomWidth/2, 
     0);
 
-  rect(
-    randomXValue, 
-    randomYValue, 
-    randomWidth, 
-    randomHeight);
+  englandWalesDemand = dataTable.getString(randomSettlementDate, 4);
+  console.log(englandWalesDemand);
+  if(englandWalesDemand > 20000){
+    stroke(255, 0, 0);
+    strokeWeight(1);
+    rect(
+      randomXValue, 
+      randomYValue, 
+      randomWidth, 
+      randomHeight);
 
-  englandWalesDeman = dataTable.getString(Math.round(random(1, dataTable.getRowCount())), 4);
-  embeddedWindGeneration = dataTable.getString(Math.round(random(1, dataTable.getRowCount())), 5);
-  embeddedWindCapacity = dataTable.getString(Math.round(random(1, dataTable.getRowCount())), 6);
-  embeddedSolorGeneration = dataTable.getString(Math.round(random(1, dataTable.getRowCount())), 7);
-  embeddedSolorCapacity = dataTable.getString(Math.round(random(1, dataTable.getRowCount())), 8);
-  
-  console.log(englandWalesDeman); 
+    fill(255, 0, 0);
+    strokeWeight(.5);
+    text(englandWalesDemand, (randomXValue + randomWidth/2) - 10, randomYValue + randomHeight/2);
+  }
+  else{
+    stroke(255);
+    strokeWeight(1);
+    rect(
+      randomXValue, 
+      randomYValue, 
+      randomWidth, 
+      randomHeight);
+
+    fill(255);
+    strokeWeight(.5);
+    text(englandWalesDemand, (randomXValue + randomWidth/2) - 10, randomYValue + randomHeight/2);
+  }
+
+  embeddedWindGeneration = dataTable.getString(randomSettlementDate, 5);
+  embeddedWindCapacity = dataTable.getString(randomSettlementDate, 6);
+  embeddedSolorGeneration = dataTable.getString(randomSettlementDate, 7);
+  embeddedSolorCapacity = dataTable.getString(randomSettlementDate, 8);
 
   textSize(10);
   fill(255);
+  stroke(255);
+  strokeWeight(.5);
+
   text(embeddedWindGeneration, randomXValue, randomYValue - 10);
   text(embeddedWindCapacity, (randomXValue + randomWidth) - 20, randomYValue - 10);
   text(embeddedSolorGeneration, randomXValue, (randomYValue + randomHeight) + 15);
