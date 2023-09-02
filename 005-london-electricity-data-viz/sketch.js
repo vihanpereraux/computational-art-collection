8a4850a1c7cb07af3f6c12d4422bf00dd0a6c4d0
Vihan Perera <vihanpereraux@gmail.com>
2023-09-02 08:39:36 +0100

    005 - Add block 02 function

--- 005-london-electricity-data-viz/sketch.js
+++ 005-london-electricity-data-viz/sketch.js
@@ -17,13 +17,14 @@ function preload(){
 }
 
 function setup() {
-  frameRate(6);  
+  frameRate(8);  
   createCanvas(360, 640);
 }
 
 function draw() {
   clear();
   blockOne();
+  // blockTwo();
 }
 
 function blockOne(){
@@ -35,6 +36,92 @@ function blockOne(){
   randomYValue = random(170, 210);
   randomWidth = random(80, 110);
   randomHeight = random(80, 110);
+
+  // get relative pointsss : ) 
+  randomSettlementDate = Math.round(random(1, dataTable.getRowCount()));
+
+  // line one
+  line(
+    0, 
+    randomYValue + randomHeight/2, 
+    randomXValue, 
+    randomYValue + randomHeight/2);
+
+  // line two
+  line(
+    randomXValue + randomWidth/2, 
+    randomYValue + randomHeight, 
+    randomXValue + randomWidth/2, 
+    height);
+
+  // line three
+  line(
+    randomXValue + randomWidth, 
+    randomYValue + randomHeight/2, 
+    width, 
+    randomYValue + randomHeight/2);
+
+  // line four
+  line(
+    randomXValue + randomWidth/2, 
+    randomYValue, 
+    randomXValue + randomWidth/2, 
+    0);
+
+  englandWalesDemand = dataTable.getString(randomSettlementDate, 4);
+  console.log(englandWalesDemand);
+  if(englandWalesDemand > 20000){
+    stroke(255, 0, 0);
+    strokeWeight(1);
+    rect(
+      randomXValue, 
+      randomYValue, 
+      randomWidth, 
+      randomHeight);
+
+    fill(255, 0, 0);
+    strokeWeight(.5);
+    text(englandWalesDemand, (randomXValue + randomWidth/2) - 10, randomYValue + randomHeight/2);
+  }
+  else{
+    stroke(255);
+    strokeWeight(1);
+    rect(
+      randomXValue, 
+      randomYValue, 
+      randomWidth, 
+      randomHeight);
+
+    fill(255);
+    strokeWeight(.5);
+    text(englandWalesDemand, (randomXValue + randomWidth/2) - 10, randomYValue + randomHeight/2);
+  }
+
+  embeddedWindGeneration = dataTable.getString(randomSettlementDate, 5);
+  embeddedWindCapacity = dataTable.getString(randomSettlementDate, 6);
+  embeddedSolorGeneration = dataTable.getString(randomSettlementDate, 7);
+  embeddedSolorCapacity = dataTable.getString(randomSettlementDate, 8);
+
+  textSize(10);
+  fill(255);
+  stroke(255);
+  strokeWeight(.5);
+
+  text(embeddedWindGeneration, randomXValue, randomYValue - 10);
+  text(embeddedWindCapacity, (randomXValue + randomWidth) - 20, randomYValue - 10);
+  text(embeddedSolorGeneration, randomXValue, (randomYValue + randomHeight) + 15);
+  text(embeddedSolorCapacity, (randomXValue + randomWidth) - 25, (randomYValue + randomHeight) + 15);
+}
+
+function blockTwo(){
+  noFill();
+  stroke(255);
+  strokeWeight(.7);
+
+  randomXValue = random(110, 150);
+  randomYValue = random(120, 150);
+  randomWidth = random(90, 120);
+  randomHeight = random(90, 120);
 
   // get relative pointsss : ) 
   randomSettlementDate = Math.round(random(1, dataTable.getRowCount()));
--- 005-london-electricity-data-viz/visual-effect/script.js
+++ 005-london-electricity-data-viz/visual-effect/script.js
@@ -1,7 +1,24 @@
 var width = 360;
 var height = 640;
 
-function dummy() {
+var waterModel = new WaterModel(width, height, {
+    resolution: 2.0,
+    // interpolate:false,
+    interpolate: true,
+    damping: 0.985,
+    clipping: 5,
+    evolveThreshold: 0.05,
+    maxFps: 30,
+    showStats: true
+});
+
+var finger = [
+    [0.5, 1.0, 0.5],
+    [1.0, 1.0, 1.0],
+    [0.5, 1.0, 0.5]
+];
+
+function blockOne(imageName) {
     var waterModel = new WaterModel(width, height, {
         resolution: 2.0,
         // interpolate:false,
@@ -15,8 +32,7 @@ function dummy() {
 
     var waterCanvas = new WaterCanvas(width, height,
         "waterHolder", waterModel, {
-        // Math.floor(Math.random() * (max - min) ) + min;
-        backgroundImageUrl: "images/image_01.jpg",
+        backgroundImageUrl: "images/" + imageName,
         lightRefraction: Math.floor(Math.random() * (5 - 1) ) + 1,
         lightReflection: .01,
         maxFps: 60,
@@ -49,6 +65,37 @@ function dummy() {
     })
 }
 
+function blockTwo(imageName){
+    var waterCanvas = new WaterCanvas(width, height,
+        "waterHolder", waterModel, {
+        backgroundImageUrl: "images/" + imageName,
+        // lightRefraction: Math.floor(Math.random() * (5 - 1) ) + 1,
+        lightRefraction: 4,
+        lightReflection: .01,
+        maxFps: 60,
+        showStats: false
+    });
+
+    // Math.floor(Math.random() * (max - min) ) + min;
+    setInterval(() => {
+        for (let index = 0; index < 50; index++) {
+            waterModel.touchWater(
+                Math.floor(Math.random() * (200 - 160)) + 160,
+                // 180, 
+                Math.floor(Math.random() * (200 - 160)) + 160,
+                // 320, 
+                .5,
+                finger);
+            // waterModel.touchWater(180, 320, .5, finger);
+        }
+    }, 300);
+
+    document.getElementById('waterHolder').addEventListener('mousemove', function (event) {
+        waterModel.touchWater((event.clientX - 340), (event.clientY - 30), .5, finger);
+    });
+}
+
 setTimeout(() => {
-    dummy();
+    blockOne("image_01.jpg");
+    // blockTwo("image_02.jpg");
 }, 100);
