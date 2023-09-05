3a6e5afe12c8a0f770bb5ae25933c27db7e7ca16
Vihan Perera <vihanpereraux@gmail.com>
2023-09-05 09:31:59 +0100

    007 - Add sample video

--- 007-jesus-meets-nirvana/sketch.js
+++ 007-jesus-meets-nirvana/sketch.js
@@ -1,20 +1,37 @@
 let video;
+let video2;
 let detector;
 let detections = [];
+let playButton = document.getElementById('play-button');
+let isVideoPlaying = false;
 
 function setup() {
-  createCanvas(640, 480);
-  video = createCapture(VIDEO, videoReady);
-  video.size(640, 480);
-  video.hide();
+  createCanvas(640, 440);
+  video2 = createVideo('./samples/sample.mp4', videoReady);
+  video2.size(640, 480);
+  video2.pause();
 }
+
+playButton.addEventListener('click', function(){
+  if(!isVideoPlaying){
+    isVideoPlaying = true;
+    playButton.innerText = "Pause";
+    video2.play();
+    video2.loop();
+  }
+  else{
+    isVideoPlaying = false;
+    playButton.innerText = "Play";
+    video2.pause();
+  }
+});
 
 function videoReady() {
   detector = ml5.objectDetector('cocossd', modelReady);
 }
 
 function modelReady() {
-  detector.detect(video, gotDetections);
+  detector.detect(video2, gotDetections);
 }
 
 function gotDetections(error, results) {
@@ -28,11 +45,11 @@ function gotDetections(error, results) {
   //   results[0].x,
   //   results[0].y);
 
-  detector.detect(video, gotDetections);
+  detector.detect(video2, gotDetections);
 }
 
 function draw() {
-  image(video, 0, 0);
+  image(video2, 0, 0, 640, 480);
   // console.log(detections);
   for (let i = 0; i < detections.length; i += 1) {
     const object = detections[i];
