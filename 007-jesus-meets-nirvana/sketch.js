2399a723182dad084c4040396fe706cfd583abbe
Vihan Perera <vihanpereraux@gmail.com>
2023-09-05 12:38:03 +0100

    007 - Update script

--- 007-jesus-meets-nirvana/sketch.js
+++ 007-jesus-meets-nirvana/sketch.js
@@ -46,10 +46,12 @@ function gotDetections(error, results) {
 
 function draw() {
   image(video2, 0, 0, width, height);
+  
+  // bounderies
   for (let i = 0; i < detections.length; i += 1) {
     const object = detections[i];
     stroke(0, 255, 0);
-    strokeWeight(4);
+    strokeWeight(2);
     noFill();
     rect(
       object.x, 
@@ -60,7 +62,22 @@ function draw() {
     // labels
     noStroke();
     fill(255);
-    textSize(24);
+    textSize(12);
     text(object.label, object.x + 10, object.y + 24);
+    text(round(object.confidence, 5), object.x + object.width/2, object.y + object.height/2);
+
+    // lines
+    let lolz = Math.round(random(0, detections.length - 1));
+    let lolz1 = Math.round(random(0, detections.length - 1));
+    if(detections.length > 1){
+      console.log(detections)
+      stroke(255);
+      strokeWeight(1.5);
+      line(
+        (detections[lolz1].width)/2 + detections[lolz1].x, 
+        (detections[lolz1].height)/2 + detections[lolz1].y,
+        (detections[lolz].width)/2 + detections[lolz].x, 
+        (detections[lolz].height)/2, + detections[lolz].y)
+      }
   }
 }
