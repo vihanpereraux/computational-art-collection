d6af5a34525091fa694bf6ed6d3f66edc000590c
Vihan Perera <vihanpereraux@gmail.com>
2023-09-07 14:52:44 +0100

    007 - Update cables effect and canvas effects

--- 007-jesus-meets-nirvana/sketch.js
+++ 007-jesus-meets-nirvana/sketch.js
@@ -10,7 +10,7 @@ let eyes1;
 let eyes2; 
 
 function preload(){
-  for (let i = 0; i < 6; i++) {
+  for (let i = 0; i < 8; i++) {
     img = loadImage('./samples/images/image_0' + String(i + 1) + '.png');
     images.push(img)
   }
@@ -28,8 +28,8 @@ function setup(){
   noStroke();
   rect(0, 0, width, height)
 
-  ears1 = {x: 200, y: height * 0.3}
-  ears2 = {x: 300, y: height * 0.3}
+  ears1 = {x: 180, y: height * 0.3}
+  ears2 = {x: 320, y: height * 0.3}
   tongue = {x: 250, y: height * 0.37}
   eyes1 = {x: 220, y: height * 0.25}
   eyes2 = {x: 290, y: height * 0.25}
@@ -38,85 +38,111 @@ function setup(){
 function draw(){
   clear();
 
-  // Effect B
-  imageSize = width * 0.33;
-  image(
-    images[round(random(0, 5))], 
-    265, 
-    height * .62, 
-    imageSize, 
-    imageSize);
+  effectB()
+  
+  effectA()
+}
 
-  // Effect A
+function effectA(){
   noFill();
   stroke(255);
-  // circle(250, height * 0.25, 248);
 
-    // ears
-    fill(255);
-    circle(ears1.x, ears1.y, 5);
-    circle(ears2.x, ears2.y, 5);
+  // ears
+  fill(255);
+  circle(ears1.x, ears1.y, 5);
+  noFill();
+  circle(ears1.x, ears1.y, 25);
+  
+  fill(255);
+  circle(ears2.x, ears2.y, 5);
+  noFill();
+  circle(ears2.x, ears2.y, 25);
 
-    // tongue
-    circle(tongue.x, tongue.y, 5);
+  // tongue
+  fill(255);
+  circle(tongue.x, tongue.y, 5);
+  noFill();
+  circle(tongue.x, tongue.y, 25);
 
-    // eyes
-    circle(eyes1.x, eyes1.y, 5);
-    circle(eyes2.x, eyes2.y, 5);
+  // eyes
+  fill(255);
+  circle(eyes1.x, eyes1.y, 5);
+  noFill();
+  circle(eyes1.x, eyes1.y, 25);
 
-    // touch
-    circle(100, height * 0.36, 5);
+  fill(255);
+  circle(eyes2.x, eyes2.y, 5);
+  noFill();
+  circle(eyes2.x, eyes2.y, 25);
 
-    // touch2
-    circle(350, height * 0.72, 5);
+  // touch
+  fill(255);
+  circle(48, height * 0.35, 5);
+  noFill();
+  circle(48, height * 0.35, 25);
+  // touch2
+  fill(255);
+  circle(440, height * 0.7, 5);
+  noFill();
+  circle(440, height * 0.7, 25);
 
-    let cordPool1 = [
-      {x: 100, y: height * 0.36},
-      {x: 350, y: height * 0.72},
-    ];
-    let cordPool2 = [
-      {x: eyes1.x, y: eyes1.y},
-      {x: eyes2.x, y: eyes2.y},
-    ];
+  let cordPool1 = [
+    {x: 48, y: height * 0.35},
+    {x: 440, y: height * 0.7},
+  ];
+  let cordPool2 = [
+    {x: eyes1.x, y: eyes1.y},
+    {x: eyes2.x, y: eyes2.y},
+  ];
 
-    // triangle(
-    noFill();
-    if(localStorage.getItem('body/touch') == 'true'){
-      let pointThree = {x:250, y:height * 0.37}
-      let randomVal = round(random(0, cordPool2.length-1)); 
-      strokeWeight(1.5);
-      triangle(
-        cordPool1[randomVal].x, cordPool1[randomVal].y,
-        cordPool2[randomVal].x, cordPool2[randomVal].y,
-        pointThree.x, pointThree.y
-        );
-    }
-    else{
-      noStroke();
-      triangle(
-        100, height * 0.36,
-        200, height * 0.3,
-        250, height * 0.37
+  // triangle(
+  noFill();
+  if(localStorage.getItem('body/touch') == 'true'){
+    let pointThree = {x:250, y:height * 0.37}
+    let randomVal = round(random(0, cordPool2.length-1)); 
+    strokeWeight(1.5);
+    triangle(
+      cordPool1[randomVal].x, cordPool1[randomVal].y,
+      cordPool2[randomVal].x, cordPool2[randomVal].y,
+      pointThree.x, pointThree.y
       );
-    }
+  }
+  else{
+    noStroke();
+    triangle(
+      100, height * 0.36,
+      200, height * 0.3,
+      250, height * 0.37
+    );
+  }
 
-    if(localStorage.getItem('eyes') == 'true'){
-      stroke(255);
-      strokeWeight(1.5);
-      line(eyes1.x, eyes1.y, eyes2.x, eyes2.y);
-    }
-    else{
-      noStroke();
-      line(eyes1.x, eyes1.y, eyes2.x, eyes2.y);
-    }
+  if(localStorage.getItem('eyes') == 'true'){
+    stroke(255);
+    strokeWeight(1.5);
+    line(eyes1.x, eyes1.y, eyes2.x, eyes2.y);
+  }
+  else{
+    noStroke();
+    line(eyes1.x, eyes1.y, eyes2.x, eyes2.y);
+  }
 
-    if(localStorage.getItem('ears') == 'true'){
-      stroke(255);
-      strokeWeight(1.5);
-      let randomVal = round(random(0, cordPool2.length-1)); 
-      triangle(
-        ears1.x, ears1.y,
-        cordPool2[randomVal].x, cordPool2[randomVal].y,
-        ears2.x, ears2.y);
-    }
+  if(localStorage.getItem('ears') == 'true'){
+    stroke(255);
+    strokeWeight(1.5);
+    let randomVal = round(random(0, cordPool2.length-1)); 
+    triangle(
+      ears1.x, ears1.y,
+      cordPool2[randomVal].x, cordPool2[randomVal].y,
+      ears2.x, ears2.y);
+  }
+}
+
+function effectB(){
+  imageSize = width * 0.32;
+  image(
+    images[round(random(0, 7))], 
+    175, 
+    height * .56, 
+    imageSize, 
+    imageSize);
 }
