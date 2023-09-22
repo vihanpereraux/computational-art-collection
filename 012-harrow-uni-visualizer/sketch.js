737f5bd727609e22e198df7bd3fbf56320dbe5e1
Vihan Perera <vihanpereraux@gmail.com>
2023-09-22 14:21:43 +0100

    012 - Update cables effect

--- 012-harrow-uni-visualizer/sketch.js
+++ 012-harrow-uni-visualizer/sketch.js
@@ -1,7 +1,34 @@
+let devider = 3
+let videoOne;
+let videoTwo;
+
 function setup() {
-  createCanvas(400, 400);
+  pixelDensity(3);
+
+  let customCanvas = createCanvas(1080 / devider, 1920 / devider);
+  customCanvas.position(
+    (window.innerWidth - width)/2, 
+    (window.innerHeight - height)/2);
+
+  videoOne = createVideo('./videos/clip01.mp4');
+  videoOne.size(1080 / devider, 1920 / devider);
+  videoOne.autoplay();
+  videoOne.volume(0);
+  videoOne.loop();
+  videoOne.hide();
+
+  videoTwo = createVideo('./videos/clip02.mp4');
+  videoTwo.size(1080 / devider, 1920 / devider);
+  videoTwo.autoplay();
+  videoTwo.volume(0);
+  videoTwo.loop();
+  videoTwo.hide();
 }
 
 function draw() {
-  background(220);
+  smooth();
+  image(videoTwo, 0, 0, width, height);
+  let tintValue = 200;
+  tint(tintValue, tintValue, tintValue);
+  filter(OPAQUE);
 }
